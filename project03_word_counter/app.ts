ee2e5a5da2141238376751481bed3671de8e7366
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-23 20:29:23 +0500

    Add files via upload

---
+++ project03_word_counter/app.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+const userInput = await inquirer.prompt({
+    type: "input",
+    name: "paragraph",
+    message: "Enter your paragraph to count words!"
+});
+const paraInput = userInput.paragraph;
+// for counting letters in paragraph
+const letterWithoutSpace = paraInput.replace(/\s/g, "");
+const letterCount = letterWithoutSpace.length; //count total number of letter
+//console.log(letterCount);
+//for counting words
+const word = paraInput.split(" ");
+const wordCount = word.length;
+console.log(chalk.green.italic(`Total letters in your para are ${letterCount} and total words are ${wordCount}`));
+
+
